dbc3ef72ab5f3927e72b71c815e51aa9593557c5
John Thompson <john.a.thompson@cuanschutz.edu>
2020-12-09 19:36:51 -0700

    update gui

--- loadAndVisualizeSleepLFP.m
+++ loadAndVisualizeSleepLFP.m
@@ -4,12 +4,16 @@
 
 %% resave files
 
+cd('C:\Users\johna\Desktop\testSleepLFP');
+
 madir = dir('*.mat');
 madirn = {madir.name};
 
 files2s = {'CLFP_01','CLFP_02','CLFP_03','CLFP_04',...
-    'CMacro_LFP_01','CMacro_LFP_02','CMacro_LFP_03',...
-    'CMacro_LFP_04'};
+    'CECOG_LF_2___01___Array_1___01',...
+    'CECOG_LF_2___02___Array_1___02',...
+    'CECOG_LF_2___03___Array_1___03',...
+    'CECOG_LF_2___04___Array_1___04'};
 
 for mi = 1:length(madirn)
     
@@ -29,6 +33,8 @@ end
 
 %% Load and plot
 
+cd('C:\Users\johna\Desktop\testSleepLFP\reducedfiles')
+
 madir2 = dir('*.mat');
 madirn2 = {madir2.name};
 
@@ -41,7 +47,12 @@ for mi2 = 1:length(madirn2)
     t.Padding = 'compact';
     for i = 1:4
         
-        tempLFP = eval(['CLFP_0',num2str(i)]);
+        if ismember(mi2, 1:9)
+            tempLFP = eval(['CLFP_0',num2str(i)]);
+        elseif ismember(mi2, 10:13)
+            tempLFP = eval(['CECOG_LF_2___0',num2str(i),'___Array_1___0',...
+                num2str(i)]);
+        end
         
         % Trim file
         clfpt = tempLFP(round(length(tempLFP)/2):end);
